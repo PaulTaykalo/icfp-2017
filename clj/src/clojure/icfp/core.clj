3e38042316d1a45a7d48d6c6b4c583f640b350ab
Alexander Yakushev <alex@bytopia.org>
2017-08-04 15:45:20 +0300

    [clj] Implement basic simulation server

--- clj/src/clojure/icfp/core.clj
+++ clj/src/clojure/icfp/core.clj
@@ -1,5 +1,9 @@
 (ns icfp.core
-  (:import icfp.Dummy))
+  (:import icfp.Dummy
+;           org.icfp2017.Main
+           ))
 
 (defn foobar [a]
   (str "foo " (Dummy/dummy a)))
+
+#_(Main/main (into-array String ["foo" "bar"]))

d1dd798164a6e607d0354a3d867104d164accc6d
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-08-04 16:06:01 +0300

    Add Passer punter

---
+++ src/main/kotlin/org/icfp2017/Game.kt
@@ -0,0 +1,17 @@
+package org.icfp2017
+
+sealed class Move {
+    class Claim(val punter: PunterID, source: SiteID, target: SiteID): Move()
+    class Pass(val punter: PunterID)
+}
+
+typealias SiteID = Int
+typealias PunterID = Int
+
+class Site(val id: SiteID)
+class River(val source: SiteID, val target: SiteID)
+class Map(val sites: Array<Site>, val rivers: Array<River>, val mines: Array<Int>)
+
+class Game(val punter: PunterID, val punters: Int, val map: Map) {
+    fun move(moves: Array<Move>) = Move.Pass(punter)
+}

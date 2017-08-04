f39d4212f4d8732de506096a16394105287a6fad
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-08-04 16:25:55 +0300

    Add solver

--- src/main/kotlin/org/icfp2017/Game.kt
+++ src/main/kotlin/org/icfp2017/Game.kt
@@ -2,7 +2,7 @@ package org.icfp2017
 
 sealed class Move {
     class Claim(val punter: PunterID, source: SiteID, target: SiteID): Move()
-    class Pass(val punter: PunterID)
+    class Pass(val punter: PunterID): Move()
 }
 
 typealias SiteID = Int
@@ -14,5 +14,5 @@ class River(val source: SiteID, val target: SiteID)
 class Map(val sites: Array<Site>, val rivers: Array<River>, val mines: Array<Int>)
 
 class Game(val punter: PunterID, val punters: Int, val map: Map) {
-    fun move(moves: Array<Move>) = Move.Pass(punter)
+    fun move(moves: Array<Move>): Move = Move.Pass(punter)
 }
--- src/main/kotlin/org/icfp2017/solver/Solver.kt
+++ src/main/kotlin/org/icfp2017/solver/Solver.kt
@@ -1,5 +1,14 @@
 package org.icfp2017.solver
 
-class Solver {
+import org.icfp2017.server.Server
 
+object Solver {
+    fun play(server: Server, name: String = "Lambada Punter") {
+        server.me(name) {
+            server.setup { game ->
+                server.ready(game.punter)
+                server.onMove { moves -> game.move(moves) }
+            }
+        }
+    }
 }

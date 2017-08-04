3a718f099bbc1d30297f1799c29368e17842a1e0
Paul Taykalo <tt.kilew@gmail.com>
2017-08-04 18:14:57 +0300

    server moves done

--- src/main/kotlin/org/icfp2017/server/Server.kt
+++ src/main/kotlin/org/icfp2017/server/Server.kt
@@ -1,11 +1,10 @@
+@file:Suppress("ArrayInDataClass")
+
 package org.icfp2017.server
 
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import org.icfp2017.Game
-import org.icfp2017.Move
-import org.icfp2017.PunterID
-import org.icfp2017.PunterName
+import org.icfp2017.*
 import org.icfp2017.base.StopCommand
 import java.io.InputStream
 import java.io.OutputStream
@@ -44,6 +43,16 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
   private data class MeRequest(@SerializedName("me") val me: String)
   private data class ReadyRequest(@SerializedName("ready") val ready: PunterID)
   private data class MeResponse(@SerializedName("you") val you: String)
+  private data class MoveResponse(
+      @SerializedName("claim") val claim: Claim?,
+      @SerializedName("pass") val pass: Pass?
+  )
+  private data class MovesArrayResponse(
+      @SerializedName("moves") val move: Array<MoveResponse>
+  )
+  private data class MoveArrayResponse(
+      @SerializedName("move") val moves: MovesArrayResponse
+  )
 
   private val client: Socket
   private val outputStream: OutputStream
@@ -62,8 +71,6 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
   override fun me(me: PunterName, callback: (PunterName) -> Unit) {
     send(Gson().toJson(MeRequest(me)))
     val response: MeResponse = Gson().fromJson(readString(), MeResponse::class.java)
-    println("Sent $response")
-
     callback(response.you)
 
   }
@@ -79,7 +86,13 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
   }
 
   override fun onMove(observer: (Array<Move>) -> Move) {
+    val response: MoveArrayResponse = Gson().fromJson(readString(), MoveArrayResponse::class.java)
+    val moves: Array<Move> = response.moves.move.map {
+      it.claim ?: it.pass ?: Pass(-1)
+    }.toTypedArray()
 
+    val move = observer(moves)
+    send(Gson().toJson(move))
   }
 
   override fun onInterruption(callback: (String) -> Unit) {
@@ -91,6 +104,7 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
   }
 
   private fun send(json: String) {
+    println("-> $json")
     val byteArray = json.toByteArray()
     val prefix = "${byteArray.size}:".toByteArray()
     outputStream.write(prefix)
@@ -102,6 +116,7 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
   private fun readString(): String {
     val size = readSize()
     val result = readBytes(size)
+    println("<-- $result")
     return result
   }
 
@@ -126,7 +141,7 @@ class OnlineServer(serverName: String = "punter.inf.ed.ac.uk", serverPort: Int =
         append(ch)
       }
     }
-    print("Is about to read $string")
+    println("Is about to read $string")
     return string.toInt()
   }
 

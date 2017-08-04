3915a82280b6501bb5092d34032f113ec488a918
Maxim Zholobak <ghostcheka@gmail.com>
2017-08-04 12:18:27 +0300

    [clj] Add test for link with clojure project

--- src/test/kotlin/org/icfp2017/MainKtTest.kt
+++ src/test/kotlin/org/icfp2017/MainKtTest.kt
@@ -3,6 +3,9 @@ package org.icfp2017
 import org.amshove.kluent.`should equal to`
 import org.amshove.kluent.shouldContain
 import org.junit.Test
+import icfp.Dummy
+import clojure.java.api.Clojure
+import clojure.lang.IFn
 
 class MainKtTest {
 
@@ -18,6 +21,15 @@ class MainKtTest {
     val jon = Person("Jon", "Doe")
     val list = listOf(alice, jon)
     list shouldContain jon
+  }
+
+  @Test
+  fun helloClojure () {
+    Dummy.dummy(1) `should equal to` "Dummy 1"
+    val require = Clojure.`var`("clojure.core", "require")
+    (require as IFn).invoke(Clojure.read("icfp.core"))
+    val foobar = Clojure.`var`("icfp.core", "foobar")
+    (foobar.invoke(42) as String) `should equal to` "foo Dummy 42"
   }
 
 }

8ddaf5f1ebe659cfdb41e4c50e224bdaa07187ec
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-08-05 08:53:50 +0300

    Add state aggregation to game.

--- src/main/kotlin/org/icfp2017/solver/DumbAndGreedy.kt
+++ src/main/kotlin/org/icfp2017/solver/DumbAndGreedy.kt
@@ -4,29 +4,23 @@ import org.icfp2017.*
 
 object DumbAndGreedy: Strategy {
 
-    val reachableSites: MutableSet<SiteID> = mutableSetOf()
-
     override fun move(game: Game): Move {
-        val river = game.map.rivers
+        val river = game.unownedRivers
                 .asSequence()
-                .filter { it.owner == null }
                 .filter {
-                    val sourceReached = it.source in reachableSites
-                    val targetReached = it.target in reachableSites
+                    val sourceReached = it.source in game.reachableSites
+                    val targetReached = it.target in game.reachableSites
 
                     /// Avoid cycles in graph
                     if (sourceReached && targetReached) return@filter false
 
-                    val sourceIsMine = it.source in game.map.mines
-                    val targetIsMine = it.target in game.map.mines
+                    val sourceIsMine = it.source in game.mines
+                    val targetIsMine = it.target in game.mines
 
                     return@filter sourceReached || targetReached || sourceIsMine || targetIsMine
                 }
                 // Add ranging of rivers.
                 .firstOrNull() ?: return game.pass()
-
-        reachableSites.add(river.target)
-        reachableSites.add(river.source)
 
         return game.claim(river)
     }

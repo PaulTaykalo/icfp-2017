cff9ecbb24cb043dc6ba9dd11390206599e7b405
oleg.taykalo <oleg.taykalo@gmail.com>
2017-08-06 15:39:37 +0200

    fixing allbase belong to us

--- src/main/kotlin/org/icfp2017/solver/AllYourBaseAreBelongToUsRandom.kt
+++ src/main/kotlin/org/icfp2017/solver/AllYourBaseAreBelongToUsRandom.kt
@@ -19,24 +19,24 @@ object AllYourBaseAreBelongToUsRandom : Strategy<StrategyStateWithGame>{
         val newGame = applyMoves(moves, state.game)
         graphUtils.updateState(newGame)
         val rivers = newGame.unownedRivers.toList()
-        if (rivers.isEmpty()) return Pair(newGame.pass(), state)
+        if (rivers.isEmpty()) return Pair(newGame.pass(),  StrategyStateWithGame(newGame))
 
         val baseRivers =  graphUtils.riversCloseToBases(rivers, newGame)
         if(baseRivers.isNotEmpty()){
             val claim = baseRivers.first()
             //Logger.log("base river claim $claim")
-            return Pair(newGame.claim(claim), state)
+            return Pair(newGame.claim(claim), StrategyStateWithGame(newGame))
         }
         // if all base rivers are captures, do most connected things
         val mostConnected = graphUtils.mostConnectedRivers(rivers)
         if(mostConnected.isNotEmpty()) {
             val claim = mostConnected.first()
             //Logger.log("most connected claim $claim")
-            return Pair(newGame.claim(claim), state)
+            return Pair(newGame.claim(claim), StrategyStateWithGame(newGame))
         }
 
 
         // if minimal spanning tree is captured, do whatever is left
-        return  Pair(newGame.claim(rivers.first()), state)
+        return  Pair(newGame.claim(rivers.first()), StrategyStateWithGame(newGame))
     }
 }
